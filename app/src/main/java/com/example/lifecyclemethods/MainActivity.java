889ed0895fc187e37f11f37f73a31dd5bb0336b9
kanmanikannu <114866367+kanmanikannu@users.noreply.github.com>
2024-03-14 09:12:14 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/lifecyclemethods/MainActivity.java
+++ app/src/main/java/com/example/lifecyclemethods/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.lifecyclemethods;
+package com.example.myapplicationex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG);
+        toast.show();
     }
-}
+
+    protected void onStart() {
+        super.onStart();
+        Toast toast1 = Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG);
+        toast1.show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast toast2 = Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG);
+        toast2.show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast toast3 = Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG);
+        toast3.show();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast toast4 = Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG);
+        toast4.show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast toast5 = Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG);
+        toast5.show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast6 = Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG);
+        toast6.show();
+    }
